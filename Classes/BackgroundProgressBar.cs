f65aba4925ed2bb89af2279392be1df715eec6aa
Izeawk <150315423+Izeawk@users.noreply.github.com>
2025-02-12 15:18:46 +0100

    Add Progress Bar Files and Demos Folder

---
+++ Classes/BackgroundProgressBar.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicProgressBar.Classes
+{
+    internal class BackgroundProgressBar
+    {
+        // Progress bar that only changes the background color as it moves instead of printing any characters
+    }
+}
